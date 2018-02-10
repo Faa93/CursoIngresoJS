7caf0d9beb7d487985a274c0f92da298e5223411
Faa93 <36281645+Faa93@users.noreply.github.com>
2018-02-09 22:12:26 -0300

    Segunda clase.

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -4,5 +4,19 @@ mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
+	var dividendo
+	var divisor
+	var dividir
+
+	dividendo=document.getElementById('numeroDividendo').value;
+	divisor=document.getElementById("numeroDivisor").value;
+
+	dividendo=parseInt(dividendo);
+	divisor=parseInt(divisor);
+
+	dividir=dividendo / divisor
+
+	alert("El resto es" +" "+ dividir);
 	
+
 }
