81d21a2b053a5333a02242afc0367103b6026dd0
Faa93 <36281645+Faa93@users.noreply.github.com>
2018-02-22 22:05:03 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,10 +3,19 @@ function Mostrar()
 
 	var contador=0;
 	var acumulador=0;
-
-
+	var numero;
+	var iteraciones=5;
+	var promedio;
+	while(contador<iteraciones)
+	{
+		contador=contador+1;
+		numero=prompt("ingrese numero");
+		numero=parseInt(numero);
+		acumulador=acumulador+contador;
+	}
+	promedio=acumulador/contador;
 
 document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+document.getElementById('promedio').value=promedio;
 
-}//FIN DE LA FUNCIÓN
+}//FIN DE LA FUNCIÓNz
--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,10 +1,18 @@
 function Mostrar()
 {
 
-	var contador=0;
-	var acumulador=0;
-	var respuesta='si';
+		var contador=0;
+		var acumulador=0;
+		var respuesta='si';
+		while(respuesta=='si')
+		{
+			contador=contador+1;
+			numero=prompt('Ingrese numero');
+			numero=parseInt(numero);
+			acumulador=acumulador+contador;
+			respuesta=prompt('Para continuar presionar, si');
 
+		}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
