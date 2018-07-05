e640d1eb946e9620a4dc3096c37fd8a9586e0b8e
Faa93 <36281645+Faa93@users.noreply.github.com>
2018-07-05 20:01:19 -0300

    Add files via upload
    parseInt, 
    Debuggear

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,39 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno=document.getElementById('numeroUno').value;
+	numeroUno=parseInt(numeroUno);
+	numeroDos=document.getElementById('numeroDos').value;
+	numeroDos=parseInt(numeroDos);
+
+	resultado=(numeroUno+numeroDos);
+
+	alert(resultado);
+
+	//El signo mas sirve para dos cosas, para sumar y para concatenar. Cuando le pasamos un texto, lo concatena, cuando le pasamos un numero, lo suma
+	//el parseInt le da el valor de un numero
+	//
+
+
+	/*
+	var numeroUno
+	var numeroDos
+	var sumar
+	
+	document.getElementById('numeroUno').value;
+	document.getElementById('numeroDos').value;
+
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+	
+	sumar=(numeroUno+numeroDos);
+
+	Alert("La suma es " Sumar);
+	*/
 
 }
 
