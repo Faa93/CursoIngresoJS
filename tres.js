a3afccf9a9d3047c9ca99acc1e71128a69ae3cfe
Faa93 <36281645+Faa93@users.noreply.github.com>
2018-07-05 21:58:08 -0300

    Add files via upload
    Parciales

---
+++ tres.js
@@ -0,0 +1,22 @@
+function Mostrar()
+{
+
+	var precio;
+	var porcentaje;
+	var descuento;
+	var dinero;
+
+	precio=prompt("Ingrese el precio");
+	porcentaje=prompt("Ingrese el porcentaje de descuento");
+
+	precio=parceint(precio);
+	descuento=porcentaje
+
+
+
+
+
+
+
+
+}
---
+++ uno.js
@@ -0,0 +1,13 @@
+
+function Mostrar()
+{
+	var base;
+	var altura;
+	var perimetro;
+	var superficie;
+
+	base=prompt("ingrese la base");
+
+	
+
+}
