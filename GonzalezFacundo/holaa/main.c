36d15ae220e4e2cd751eefd60016b05189a9eea2
Faa93 <36281645+Faa93@users.noreply.github.com>
2018-08-21 22:17:36 -0300

    Clase 1
    programacion

---
+++ GonzalezFacundo/holaa/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int nro1,nro2,nro3,dato;
+    printf("ingrese: ");
+    scanf("%d",& nro1);
+    printf("\nIngrese: ");
+    scanf("%d",&nro2);
+    printf("\nIngrese: ");
+    scanf("%d",&nro3);
+    dato=nro1+nro2+nro3;
+    printf("La suma es: %d", dato);
+
+    return 0;
+}
